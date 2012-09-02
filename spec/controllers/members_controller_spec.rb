ddad945f3df77af307c3d0a7c10bd8e9014371ab
Duana Saskia <starkcoffee+apps@gmail.com>
2012-09-02 17:12:54 +0400

    add test for missing family member fields

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -2,23 +2,54 @@ require 'spec_helper'
 
 describe MembersController do
 
-  let(:valid_attributes) {{
-    "doj" => "30/01/1970",
+  let(:valid_member_params) {{
+    :name => "Dania",
+    :ic_number => "123",
+    :doj => "30/01/1970",
+    :phone => "9999",
+    :donation => 20,
+    :doj => "30/01/1970",
+    "family_members_attributes" => {
+      "0"=>{"name"=>"Mustafa"},
+    }
+  }}
+  let (:valid_addr_params) {{
+    :line1 => "Foo St"
   }}
 
   describe "POST create" do
+    def do_create_with_member_params(params)
+      post :create, { 
+        :member => params,
+        :address => valid_addr_params
+      }
+    end
+
     it "ignores blank family member fields" do
-      post :create, { :member => valid_attributes.merge({
+      do_create_with_member_params valid_member_params.merge({
         "family_members_attributes" => {
           "0"=>{"name"=>"Mustafa"},
           "1"=>{"name"=>""},
           "2"=>{"name"=>""},
-        }
-      })}
+        },
+      })
 
       family_members = assigns(:member).family_members
       family_members.count.should  == 1
       family_members.first.name.should == "Mustafa"
+    end
+
+    context "there are validation errors" do
+      let(:invalid_member_params) {
+        valid_member_params.merge({ :name => "" })
+      }
+
+      it "contains entered family member fields if there were validation errors" do
+        do_create_with_member_params invalid_member_params
+
+        family_members = assigns(:member).family_members
+        family_members.count.should  == 1
+      end
     end
   end
 end
